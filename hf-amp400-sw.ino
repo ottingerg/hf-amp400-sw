a2c9f7ad93721e60e816b6e8cbe136d803c002b2
willy <willy>
2015-07-30 18:54:26 +0200

    smarter use of timeouts instead of delay in superloop + Ethernet Server

--- hf-amp400-sw.ino
+++ hf-amp400-sw.ino
@@ -21,8 +21,32 @@
 
 #include <Wire.h>
 #include "hfamp400.h"
+#include <SPI.h>
+#include <Ethernet.h>
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 0, 117);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(4711);
+
+const int led_pin = 13;
 
 uint32_t num_alerts=0;
+
+#define SERIALPRINT_INTERVAL 1000 //ms
+#define ADCREAD_INTERVAL 500 //ms
+
+
+int last_serialprint = millis() - SERIALPRINT_INTERVAL;
+int last_adcread = millis() - ADCREAD_INTERVAL;
+
 
 void alert_interrupt()
 {
@@ -42,6 +66,13 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   char response[3];
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+
+  pinMode(led_pin, OUTPUT);
+  
   hfamp400.init();
 
   //Alert Interrupt Example
@@ -129,101 +160,141 @@ void setup() {
 
 void loop() {
   uint32_t alerts;
+  char c;
   float temp[4];
   float power[4];
   float current[3]; // fuse 
   float voltage[3]; // fuse  after FET  to control proper release of fuse , also usable for dc POWER INPUT MEASUREMENTS
 
-
-  Serial.println("");
-  
-  //Read Temperatures
-  
-  hfamp400.get_temperature(TEMP_CH_D1,&temp[0]);
-  hfamp400.get_temperature(TEMP_CH_D2,&temp[1]);
-  hfamp400.get_temperature(TEMP_CH_AD7294,&temp[2]);
-  hfamp400.get_temperature(TEMP_CH_BOARD,&temp[3]);
-
-  Serial.print("Temperatures D1= ");
-  Serial.print(temp[0]);
-  Serial.print(" D2= ");
-  Serial.print(temp[1]);
-  Serial.print(" AD7294= ");
-  Serial.print(temp[2]);
-  Serial.print(" BOARD= ");
-  Serial.print(temp[3]);
-  Serial.println(" [deg C]");
  
-  //Read HF-Power
-  
-  hfamp400.get_hfpower(HF_CH_FORWARD_1,&power[0]);
-  hfamp400.get_hfpower(HF_CH_FORWARD_2,&power[1]);
-  hfamp400.get_hfpower(HF_CH_RETURN_1,&power[2]);
-  hfamp400.get_hfpower(HF_CH_RETURN_2,&power[3]);
 
-  Serial.print("HFPower Forward_1= ");
-  Serial.print(power[0]);
-  Serial.print(" Forward_2= ");
-  Serial.print(power[1]);
-  Serial.print(" Return_1= ");
-  Serial.print(power[2]);
-  Serial.print(" Return_2= ");
-  Serial.print(power[3]);
-  Serial.println(" [dBm]");
+  if((last_adcread + ADCREAD_INTERVAL) <= millis())
+  {
+    //Read Temperatures
+    
+    hfamp400.get_temperature(TEMP_CH_D1,&temp[0]);
+    hfamp400.get_temperature(TEMP_CH_D2,&temp[1]);
+    hfamp400.get_temperature(TEMP_CH_AD7294,&temp[2]);
+    hfamp400.get_temperature(TEMP_CH_BOARD,&temp[3]);
+  
+    //Read HF-Power
+    
+    hfamp400.get_hfpower(HF_CH_FORWARD_1,&power[0]);
+    hfamp400.get_hfpower(HF_CH_FORWARD_2,&power[1]);
+    hfamp400.get_hfpower(HF_CH_RETURN_1,&power[2]);
+    hfamp400.get_hfpower(HF_CH_RETURN_2,&power[3]);
+  
+  
+    //Read Voltage
+    
+    hfamp400.get_fuse_voltage(FUSE_CH_1, &voltage[0]);
+    hfamp400.get_fuse_voltage(FUSE_CH_2, &voltage[1]);
+    hfamp400.get_fuse_voltage(FUSE_CH_3, &voltage[2]);
+    
+  
+    //Read Current
+    
+    hfamp400.get_fuse_current(FUSE_CH_1, &current[0]);
+    hfamp400.get_fuse_current(FUSE_CH_2, &current[1]);
+    hfamp400.get_fuse_current(FUSE_CH_3, &current[2]);
+    
+    last_adcread=millis();
+  }   
+  
+  if((last_serialprint + SERIALPRINT_INTERVAL) <= millis())
+  {
+    Serial.println("");
+    
+    //Print Temperatures
+    Serial.print("Temperatures D1= ");
+    Serial.print(temp[0]);
+    Serial.print(" D2= ");
+    Serial.print(temp[1]);
+    Serial.print(" AD7294= ");
+    Serial.print(temp[2]);
+    Serial.print(" BOARD= ");
+    Serial.print(temp[3]);
+    Serial.println(" [deg C]");
 
+    //Print HF-Power
+    Serial.print("HFPower Forward_1= ");
+    Serial.print(power[0]);
+    Serial.print(" Forward_2= ");
+    Serial.print(power[1]);
+    Serial.print(" Return_1= ");
+    Serial.print(power[2]);
+    Serial.print(" Return_2= ");
+    Serial.print(power[3]);
+    Serial.println(" [dBm]");
 
-  //Read Voltage
-  
-  hfamp400.get_fuse_voltage(FUSE_CH_1, &voltage[0]);
-  hfamp400.get_fuse_voltage(FUSE_CH_2, &voltage[1]);
-  hfamp400.get_fuse_voltage(FUSE_CH_3, &voltage[2]);
-  
-  Serial.print("Fuse-Voltage CH1= ");
-  Serial.print(voltage[0]);
-  Serial.print(" CH2= ");
-  Serial.print(voltage[1]);
-  Serial.print(" CH3= ");
-  Serial.print(voltage[2]);
-  Serial.println(" [V]");
+    //Print Fuse Currents
 
-  //Read Current
-  
-  hfamp400.get_fuse_current(FUSE_CH_1, &current[0]);
-  hfamp400.get_fuse_current(FUSE_CH_2, &current[1]);
-  hfamp400.get_fuse_current(FUSE_CH_3, &current[2]);
-  
-  Serial.print("Fuse-Current CH1= ");
-  Serial.print(current[0]);
-  Serial.print(" CH2= ");
-  Serial.print(current[1]);
-  Serial.print(" CH3= ");
-  Serial.print(current[3]);
-  Serial.println(" [A]");
+    Serial.print("Fuse-Voltage CH1= ");
+    Serial.print(voltage[0]);
+    Serial.print(" CH2= ");
+    Serial.print(voltage[1]);
+    Serial.print(" CH3= ");
+    Serial.print(voltage[2]);
+    Serial.println(" [V]");
 
-  
-  alerts = hfamp400.ad7294.get_alerts();
-  
-  if(alerts & ALERT_MASK_TEMP_D1)
-    Serial.println("!!! ALERT: TEMP_D1");
-  if(alerts & ALERT_MASK_TEMP_D2)
-    Serial.println("!!! ALERT: TEMP_D2");
-  if(alerts & ALERT_MASK_TEMP_INT)
-    Serial.println("!!! ALERT: TEMP_INT");
-  if(alerts & ALERT_MASK_HFPOWER_RETURN_1)
-    Serial.println("!!! ALERT: HFPOWER_RETURN_1");
-  if(alerts & ALERT_MASK_HFPOWER_RETURN_2)
-    Serial.println("!!! ALERT: HFPOWER_RETURN_2");
-  if(alerts & ALERT_MASK_FUSE_1)
-    Serial.println("!!! ALERT: FUSE_1");
-  if(alerts & ALERT_MASK_FUSE_2)
-    Serial.println("!!! ALERT: FUSE_2");
+    //Print Fuse Voltages
 
-  
-  
-  delay(1000);
-      
-  hfamp400.ad7294.clear_alerts(); //alerts needs to be cleared explicitly  
-  
+    Serial.print("Fuse-Current CH1= ");
+    Serial.print(current[0]);
+    Serial.print(" CH2= ");
+    Serial.print(current[1]);
+    Serial.print(" CH3= ");
+    Serial.print(current[3]);
+    Serial.println(" [A]");
+
+   alerts = hfamp400.ad7294.get_alerts();
+    
+    if(alerts & ALERT_MASK_TEMP_D1)
+      Serial.println("!!! ALERT: TEMP_D1");
+    if(alerts & ALERT_MASK_TEMP_D2)
+      Serial.println("!!! ALERT: TEMP_D2");
+    if(alerts & ALERT_MASK_TEMP_INT)
+      Serial.println("!!! ALERT: TEMP_INT");
+    if(alerts & ALERT_MASK_HFPOWER_RETURN_1)
+      Serial.println("!!! ALERT: HFPOWER_RETURN_1");
+    if(alerts & ALERT_MASK_HFPOWER_RETURN_2)
+      Serial.println("!!! ALERT: HFPOWER_RETURN_2");
+    if(alerts & ALERT_MASK_FUSE_1)
+      Serial.println("!!! ALERT: FUSE_1");
+    if(alerts & ALERT_MASK_FUSE_2)
+      Serial.println("!!! ALERT: FUSE_2");
+
+    hfamp400.ad7294.clear_alerts(); //alerts needs to be cleared explicitly  
+
+    last_serialprint = millis();
+  }
+ 
+
+    // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+   
+    if (client.connected()) {
+      if(client.available())
+      {
+        c = client.read();
+        if(c == '1') digitalWrite(led_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+        if(c == '0') digitalWrite(led_pin, LOW);  
+        if(c == 'r') {
+          client.print(temp[0],4);
+          client.print(",");
+          client.print(temp[1],4);
+          client.print(",");
+          client.print(temp[2],4);
+          client.print(",");
+          client.println(temp[3],4);
+          
+        }
+      }
+    } else {
+      client.stop();
+    }
+  } 
    
 }
 
